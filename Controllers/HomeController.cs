18e9d68d9b91dff73c598458d5c839c9b1f679d4
Jeremy Kale Padot <jeremy@padot.us>
2020-03-30 11:29:19 -0700

    create basic boilerplate with home ctrl

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allArticles = Article.GetArticles("[QJmUAg80RmZlE8EF0Nbhq5smFHmX0zqA]");
+            return View(allArticles);
+        }
+    }
+}
